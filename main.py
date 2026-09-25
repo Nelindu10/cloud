66647b03f2cf8140486cc084af9a49d72b38286d
Nelindu Dahamitha <dahamithawickramasinghe@gmail.com>
2026-09-25 11:06:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# MongoDB Connect
+client = MongoClient("mongodb+srv://dahamithawickramasinghe_db_user:okdRrm2scbqGUvQA@cluster0.uiwpse0.mongodb.net/?appName=Cluster0")
+db = client["smartmove_db"]
+passengers_collection = db["passengers"]
+
+
+@app.route("/")
+def home():
+    return "<h1>Welcome to SmartMove Home Page!</h1><br><a href='/register'>Go to Register Page</a>"
+
+
+# GET and POST include
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    # register una kiyanna variable
+    success_message = False
+
+    if request.method == "POST":
+        # HTML form eke data ganima
+        name = request.form.get("name")
+        email = request.form.get("email")
+        password = request.form.get("psw")
+
+        # data Dictionary lesa hadima
+        passenger_data = {
+            "name": name,
+            "email": email,
+            "password": password
+        }
+
+        # haduwa MongoDB collection eka insert kara
+        passengers_collection.insert_one(passenger_data)
+        success_message = True
+
+    # MongoDB eken okkoma data gannawa
+    passengers_list = list(passengers_collection.find())
+
+    # data ha form eka thiyana ewa penwima
+    return render_template("index.html", passengers=passengers_list, success=success_message)
+
+
+@app.route("/login")
+def login():
+    return "<h1>Login Page - Next to update hode!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
